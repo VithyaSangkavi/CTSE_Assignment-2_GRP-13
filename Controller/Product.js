71c16f8e4ab60dbc36267ffb8655bbd9b588948e
VithyaSangkavi <vithyasangkavi@gmail.com>
2024-04-11 16:47:12 +0530

    Created getProductById and getAllProducts methods

--- Controller/Product.js
+++ Controller/Product.js
@@ -48,4 +48,35 @@ const updateProduct = async (req, res) => {
   }
 };
 
-module.exports = { addNewProduct, updateProduct };
+/**
+ * Get a product by its ID from the database.
+ */
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+/**
+ * Get all products from the database.
+ */
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { addNewProduct, updateProduct, getProductById, getAllProducts };
--- Route/Product.js
+++ Route/Product.js
@@ -4,5 +4,7 @@ const Product = require("../Controller/Product");
 
 router.route("/add").post(Product.addNewProduct);
 router.route("/update/:id").put(Product.updateProduct);
+router.route("/:id").get(Product.getProductById);
+router.route("/").get(Product.getAllProducts);
 
 module.exports = router;
